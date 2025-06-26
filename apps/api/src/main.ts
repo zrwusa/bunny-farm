da4e095f20bcd68fc7a24e25d62fa270e9f6b7bb
Revone <zrwusa@gmail.com>
2025-06-26 21:27:15 +1200

    release: app listen 0.0.0.0

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -28,7 +28,8 @@ async function bootstrap() {
   );
 
   // 5. Start the service
-  await app.listen(process.env.PORT ?? 8080);
+  const port = parseInt(process.env.PORT ?? '8080', 10);
+  await app.listen(port, '0.0.0.0');
 }
 
 bootstrap().then();
